a7c3ad35aa006eaaf5fd902668bebc4487719abf
danendrafidel <fidel.khanza@gmail.com>
2024-11-10 23:52:29 +0700

    add message

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -5,10 +5,14 @@ export class BookController {
   async addBook(req: Request, res: Response, next: NextFunction) {
     try {
       const book = await BookService.addBook(req.body);
-      res.status(201).json(book);
+      res.status(201).json({
+        status: "success",
+        message: "Book added successfully",
+        data: book,
+      });
       return;
     } catch (error: any) {
-      res.status(400).json({ message: error.message });
+      res.status(400).json({ status: "error", message: error.message });
       return;
     }
   }
@@ -16,9 +20,13 @@ export class BookController {
   async getAllBooks(req: Request, res: Response) {
     try {
       const books = await BookService.getAllBooks();
-      res.json(books);
+      res.json({
+        status: "success",
+        message: "Successfully get all book",
+        data: books,
+      });
     } catch (error: any) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ status: "error", message: error.message });
     }
   }
 
@@ -26,26 +34,36 @@ export class BookController {
     try {
       const book = await BookService.getBookById(req.params.id);
       if (!book) {
-        res.status(404).json({ message: "Book not found" });
+        res.status(404).json({ status: "error", message: "Book not found" });
         return;
       }
-      res.json(book);
+      res.json({
+        status: "success",
+        message: "Successfully get book",
+        data: book,
+      });
     } catch (error: any) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ status: "error", message: error.message });
     }
   }
 
   async modifyBook(req: Request, res: Response) {
     try {
       const book = await BookService.modifyBook(req.params.id, req.body);
-      res.json(book);
+      res.json({
+        status: "success",
+        message: "Successfully update book",
+        data: book,
+      });
     } catch (error: any) {
       if (error.message.includes("Invalid book ID format")) {
-        res.status(400).json({ message: error.message });
+        res.status(400).json({ status: "error", message: error.message });
       } else if (error.message.includes("not found")) {
-        res.status(404).json({ message: error.message });
+        res.status(404).json({ status: "error", message: error.message });
       } else {
-        res.status(500).json({ message: "Internal server error" });
+        res
+          .status(500)
+          .json({ status: "error", message: "Internal server error" });
       }
     }
   }
@@ -53,14 +71,20 @@ export class BookController {
   async removeBook(req: Request, res: Response) {
     try {
       const book = await BookService.removeBook(req.params.id);
-      res.json({ message: "Book deleted successfully", book });
+      res.json({
+        status: "success",
+        message: "Successfully remove book",
+        data: book,
+      });
     } catch (error: any) {
       if (error.message.includes("Invalid book ID format")) {
-        res.status(400).json({ message: error.message });
+        res.status(400).json({ status: "error", message: error.message });
       } else if (error.message.includes("not found")) {
-        res.status(404).json({ message: error.message });
+        res.status(404).json({ status: "error", message: error.message });
       } else {
-        res.status(500).json({ message: "Internal server error" });
+        res
+          .status(500)
+          .json({ status: "error", message: "Internal server error" });
       }
     }
   }
